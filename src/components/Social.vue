6ddabfff4e79e6dceca276ee140c9588527aa7f6
Nicolas Gabbani <nicolasgabbani@gmail.com>
2021-03-01 12:46:49 +0100

    fix discord link

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="resos">
     <a class="twitter animate__animated animate__bounceIn animate__delay-1s" href="https://twitter.com/Krank_Haus"><i class="fab fa-twitter"></i></a>
-    <a class="discord animate__animated animate__bounceIn animate__delay-2s" href="https://discord.gg/"><i class="fab fa-discord"></i></a>
+    <a class="discord animate__animated animate__bounceIn animate__delay-2s" href="https://discord.gg/TYtGnHd"><i class="fab fa-discord"></i></a>
     <a class="instagram animate__animated animate__bounceIn animate__delay-3s" href="https://www.instagram.com/krank_haus/"><i class="fab fa-instagram"></i></a>
     <a class="twitch animate__animated animate__bounceIn animate__delay-4s" href="https://www.twitch.tv/krankhaus"><i class="fab fa-twitch"></i></a>
   </div>
